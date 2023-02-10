616ce51c1de51222af2d97e0a644531ea606b3eb
Aniket Rai <arai@palantir.com>
2023-02-10 05:30:26 +0000

    Shell configuration dotfiles

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ sudo apt-get update
 sudo apt-get install -y fish
 sudo chsh -s /usr/bin/fish $USER
 
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh > install-starship.sh
+chmod +x install-starship.sh
+./install-starship.sh --yes
 
 mkdir ~/.config
 cp starship.toml ~/.config/starship.toml
