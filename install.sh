63fdcae5c9fa50b85b9409e49d39837c658ba95b
Aniket Rai <arai@palantir.com>
2023-02-10 05:12:31 +0000

    Shell configuration

--- install.sh
+++ install.sh
@@ -3,12 +3,11 @@
 echo "-- Starting shell personalisation"
 
 sudo apt-get update
-sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get install -y fish
 sudo chsh -s /usr/bin/fish $USER
 
 curl -sS https://starship.rs/install.sh | sh
 
-mkdir "~/.config/"
-cp starhip.toml ~/.config/starship.toml
-cp -r "fish/" "~/.config/"
+mkdir ~/.config
+cp starship.toml ~/.config/starship.toml
+cp -r fish/ ~/.config/fish/
